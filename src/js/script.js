2d83f5e6f9666259c21cd2dced265fd7b353ed41
Irina <irina.novysh@yandex.by>
2021-09-29 21:26:48 +0300

    Add modal

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,9 @@ window.addEventListener("DOMContentLoaded", function () {
   let promoClose = document.querySelector(".promo__close");
   let nav = document.querySelector(".promo__nav");
   let pageUp = document.querySelector(".pageUp");
-
+  let btnModal = document.querySelectorAll(".btn_modal");
+  let modal = document.querySelector(".modal");
+  let close = document.querySelector(".close");
   hamburger.addEventListener("click", function () {
     nav.style.right = 0;
   });
@@ -11,7 +13,14 @@ window.addEventListener("DOMContentLoaded", function () {
   promoClose.addEventListener("click", function () {
     nav.style.right = "-100%";
   });
-
+  btnModal.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      modal.style.display = "flex";
+      let height = window.pageYOffset;
+      modal.style.top = height + "px";
+      document.body.style.overflow = "hidden";
+    });
+  });
   window.addEventListener("scroll", function () {
     let scrollH = window.pageYOffset;
     if (scrollH > 1000) {
@@ -19,5 +28,9 @@ window.addEventListener("DOMContentLoaded", function () {
     } else {
       pageUp.style.display = "none";
     }
+  });
+  close.addEventListener("click", function () {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto";
   });
 });
